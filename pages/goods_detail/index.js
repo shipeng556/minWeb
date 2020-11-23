5ef8fe86e1b193d1d1ffc966586fbaa338f616c4
shipeng556 <1013404683@qq.com>
2020-11-23 15:19:38 +0800

    完成加入购物车功能

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -2,8 +2,17 @@
 // 2、点击轮播图 预览大图
 //    1 给轮播图绑定点击事件
 //    2 调用小程序的api previewImage
+// 3 点击 加入购物车
+//   1 先绑定点击事件
+//   2 获取缓存中的购物车数据 数组格式
+//   3 先判断 当前的商品是否已经存在于 购物车
+//   4 已经存在 修改商品数据 执行购物车数量++ 重新把购物车数组 填充会缓存中
+//   5 不存在于购物车的数组中 直接给购物车数组添加一个新元素 新元素 带上购买 数量属性 num 重新把购物车数组 填充会缓存中
+//   6 弹出提示
 
-import { request } from "../../request/index.js"
+import {
+  request
+} from "../../request/index.js"
 Page({
 
   /**
@@ -20,14 +29,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const { goods_id } = options;
+    const {
+      goods_id
+    } = options;
     console.log(goods_id);
     this.getGoodsDetail(goods_id);
 
   },
   // 获取商品详情的数据
   async getGoodsDetail(goods_id) {
-    const goodsObj = await request({ url: "/goods/detail", data: { goods_id } })
+    const goodsObj = await request({
+      url: "/goods/detail",
+      data: {
+        goods_id
+      }
+    })
     this.goodsInfo = goodsObj;
     this.setData({
       goodsObj: {
@@ -52,7 +68,30 @@ Page({
       current,
       urls
     });
-  }
-
+  },
+  // 点击家人购物车
+  handleCartAdd() {
+    // 1 获取缓存中的购物车 数组
+    let cart = wx.getStorageSync("cart") || [];
+    // 2
+    let index = cart.findIndex(v => v.goods_id === this.goodsInfo.goods_id);
+    if (index === -1) {
+      // 3 不存在该商品 第一次添加
+      this.goodsInfo.num = 1;
+      cart.push(this.goodsInfo);
+    } else {
+      // 4 已经存在购物车 执行 num++
+      cart[index].num++;
+    }
+    // 5 把购物车数据 重新添加会缓存中
+    wx.setStorageSync("cart", cart);
+    // 6 弹窗提示
+    wx.showToast({
+      title: '加入成功！',
+      icon: 'success',
+      // true 防止用户手抖 疯狂点击
+      mask: true,
+    });
+  },
 
 })
