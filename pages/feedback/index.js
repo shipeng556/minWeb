2dd4e7d54dd0639bb6437fa06508cc829ee264f7
shipeng556 <1013404683@qq.com>
2020-12-08 17:27:04 +0800

    意见反馈功能开发

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,66 +1,84 @@
-// pages/feedback/index.js
+// 1 点击 ‘+’ 触发tap 点击事件
+//   1 调用小程序内置的 选择图片的 api
+//   2 获取到 图片的路径 数组
+//   3 把图片路径 存到 data 的变量中
+//   4 页面就可以根据 图片数组 进行循环遍历 自定义组件
+// 2 点击 自定义图片 组件
+//   1 获取被点击的元素的索引
+//   2 获取 data 中的图片数组
+//   3 根据索引  删除数组中对于的元素
+//   4 把数组重新设置回data中
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
+    tabs: [{
+        id: 0,
+        value: "意见反馈",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "品牌、商家投诉",
+        isActive: false
+      }
+    ],
+    // 图片数组
+    chooseImgs: []
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  handleTabsItemChange(e) {
+    //1、获取被点击的标题索引
+    const {
+      index
+    } = e.detail;
+    // 2、修改源数组
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    // 3、赋值到data中
+    this.setData({
+      tabs
+    })
+  },
+  // 点击上传图片
+  handleChooseImg() {
+    wx.chooseImage({
+      // 上传图片的张数
+      count: 9,
+      // 图片的格式   原图   压缩图片
+      sizeType: ['original', 'compressed'],
+      // 图片的来源  相册   照相机
+      sourceType: ['album', 'camera'],
+      success: (result) => {
+        console.log(result);
+        this.setData({
+          // 图片数组拼接
+          chooseImgs: [...this.data.chooseImgs, ...result.tempFilePaths]
+        })
+
+      }
+    });
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击 删除图片
+  handleRemoveImg(e) {
+    // 获取索引
+    const {
+      index
+    } = e.currentTarget.dataset;
+    // 获取data 中的图片数组
+    let {
+      chooseImgs
+    } = this.data;
+    // 删除元素
+    chooseImgs.splice(index, 1);
+    this.setData({
+      chooseImgs
+    })
   }
+
+
 })
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,71 @@
-// pages/search/index.js
+// 1 输入框绑定 值改变事件 input事件
+//   1 获取到输入框的值
+//   2 合法性判断
+//   3 检验通过 把输入框的值 发送到后台
+//   4 返回的数据打印到页面上
+// 2 防抖 （防止抖动） 定时器 节流
+//   1 防抖 一般 输入框中 防止重复输入 重复发送请求
+//   2 节流 一般是用在页面下拉和上拉
+//   3 定义全局的定时器id
+
+import {
+  request
+} from "../../request/index.js"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    goods: [],
+    // 取消 按钮 是否显示
+    isFocus:false,
+    // 输入框的值
+    inpValue:""
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  TimeId: -1,
+  // 输入框的值改变 就会触发的事件
+  handleInput(e) {
+    // 1 获取输入框的值
+    const {
+      value
+    } = e.detail;
+    console.log(value);
+    // 2 检测合法性
+    if (!value.trim()) {
+      return;
+    }
+    this.setData({
+      isFocus:true
+    })
+    // 3 发送请求获取数据
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.qSearch(value);
+    }, 1000);
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  async qSearch(query) {
+    const res = await request({
+      url: "/goods/qsearch",
+      data: {
+        query
+      }
+    })
+    console.log(res);
+    this.setData({
+      goods: res
+    })
 
   },
+  // 点击 取消按钮
+  handleCancel(){
+    this.setData({
+      inpValue:"",
+      isFocus:false,
+      goods:[]
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    })
 
   }
+
+
 })
